dad3666c0ba8f85363e0e3d05993a29f4335f311
falguni-integral <falguni.gondnale@integral.io>
2020-03-26 20:09:51 -0400

    inital commit

---
+++ src/test/java/com/example/integral/todoAppdemo/TodoAppDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.integral.todoAppdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class TodoAppDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+    TodoAppDemoApplication.main(new String[]{});
+  }
+
+}
