20aba8555626944b4d2f5cf4bbff8b010fdb0c5a
falgunigo <falguni.gondnale>
2020-04-13 19:11:44 -0400

    Create command line spring boot application to print ToDo tasks

--- src/main/java/com/example/integral/todoAppdemo/TodoAppDemoApplication.java
+++ src/main/java/com/example/integral/todoAppdemo/TodoAppDemoApplication.java
@@ -1,15 +1,34 @@
 package com.example.integral.todoAppdemo;
 
+import java.time.LocalDateTime;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TodoAppDemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TodoAppDemoApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(TodoAppDemoApplication.class, args);
+  }
 
-	void run(String... args){
-	}
+  @Bean
+  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+    return args -> {
+      ToDoTask aTask = new ToDoTask("task", LocalDateTime.now());
+      ToDoTask simpleTask = new ToDoTask("simple Task", LocalDateTime.now());
+
+      TaskCollector taskCollector = new TaskCollector();
+      taskCollector.add(aTask);
+      taskCollector.add(simpleTask);
+
+      for (ToDoTask task : taskCollector.getAllTasks()) {
+        System.out
+            .println(String.format("Task: %s, Created: %s", task.getName(), task.getCreated()));
+      }
+    };
+  }
 }
+
