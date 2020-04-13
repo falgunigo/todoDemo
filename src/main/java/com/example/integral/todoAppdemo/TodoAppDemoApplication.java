1003a3c7918607ce0a32303f5c1bfcb3a483ab0e
falgunigo <falguni.gondnale>
2020-04-13 19:28:46 -0400

    Application implements CommandLineRunner to create a console application.

--- src/main/java/com/example/integral/todoAppdemo/TodoAppDemoApplication.java
+++ src/main/java/com/example/integral/todoAppdemo/TodoAppDemoApplication.java
@@ -4,31 +4,27 @@ import java.time.LocalDateTime;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class TodoAppDemoApplication {
+public class TodoAppDemoApplication implements CommandLineRunner {
 
   public static void main(String[] args) {
     SpringApplication.run(TodoAppDemoApplication.class, args);
   }
 
-  @Bean
-  public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
-    return args -> {
-      ToDoTask aTask = new ToDoTask("task", LocalDateTime.now());
-      ToDoTask simpleTask = new ToDoTask("simple Task", LocalDateTime.now());
+	@Override
+	public void run(String... args) throws Exception {
+		ToDoTask aTask = new ToDoTask("task", LocalDateTime.now());
+		ToDoTask simpleTask = new ToDoTask("simple Task", LocalDateTime.now());
 
-      TaskCollector taskCollector = new TaskCollector();
-      taskCollector.add(aTask);
-      taskCollector.add(simpleTask);
+		TaskCollector taskCollector = new TaskCollector();
+		taskCollector.add(aTask);
+		taskCollector.add(simpleTask);
 
-      for (ToDoTask task : taskCollector.getAllTasks()) {
-        System.out
-            .println(String.format("Task: %s, Created: %s", task.getName(), task.getCreated()));
-      }
-    };
-  }
+		for (ToDoTask task : taskCollector.getAllTasks()) {
+			System.out
+					.println(String.format("Task: %s, Created: %s", task.getName(), task.getCreated()));
+		}
+	}
 }
 
